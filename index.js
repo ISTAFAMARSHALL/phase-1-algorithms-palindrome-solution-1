378e5918d8f8e4f66824312858c59e01c189f756
+ <+>
2022-06-30 13:44:45 -0400

    commit tree 192668d4dead83fffb7f12de22049731acd1c0b8

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+
+function reverse(word) {
+
+  return word.split("").reverse().join("");
+
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+
+  const reversedWord = reverse(word)
+
+  return word === reversedWord
+
 }
 
 /* 
@@ -23,3 +35,33 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
